4f313460a72e7f79cd5007ea95df9a84527dd83a
changlz <changlizhi@aliyun.com>
2017-09-14 10:57:37 +0800

    创建日期字段

--- sjkmhsydata/Biaolie.go
+++ sjkmhsydata/Biaolie.go
@@ -4,15 +4,30 @@ import (
 	"changliang/biaolie"
 )
 
-func (sjkmhsydata *Sjkmhsydata) JueseId(xiaoxie bool) string {
-	return biaolie.Bls.Juese(xiaoxie) + biaolie.Bls.Id(xiaoxie)
+func (sjkmhsydata *Sjkmhsydata) DtziyuanId(xiaoxie bool) string {
+	return biaolie.Bls.Dtziyuan(xiaoxie) + biaolie.Bls.Id(xiaoxie)
 }
-func (sjkmhsydata *Sjkmhsydata) JueseBiaoji(xiaoxie bool) string {
-	return biaolie.Bls.Juese(xiaoxie) + biaolie.Bls.Biaoji(xiaoxie)
+func (sjkmhsydata *Sjkmhsydata) DtziyuanMingcheng(xiaoxie bool) string {
+	return biaolie.Bls.Dtziyuan(xiaoxie) + biaolie.Bls.Mingcheng(xiaoxie)
 }
-func (sjkmhsydata *Sjkmhsydata) JueseBianma(xiaoxie bool) string {
-	return biaolie.Bls.Juese(xiaoxie) + biaolie.Bls.Bianma(xiaoxie)
+func (sjkmhsydata *Sjkmhsydata) DtziyuanPaixu(xiaoxie bool) string {
+	return biaolie.Bls.Dtziyuan(xiaoxie) + biaolie.Bls.Paixu(xiaoxie)
 }
-func (sjkmhsydata *Sjkmhsydata) JueseMingcheng(xiaoxie bool) string {
-	return biaolie.Bls.Juese(xiaoxie) + biaolie.Bls.Mingcheng(xiaoxie)
+func (sjkmhsydata *Sjkmhsydata) DtziyuanBianma(xiaoxie bool) string {
+	return biaolie.Bls.Dtziyuan(xiaoxie) + biaolie.Bls.Bianma(xiaoxie)
+}
+func (sjkmhsydata *Sjkmhsydata) DtziyuanFubianma(xiaoxie bool) string {
+	return biaolie.Bls.Dtziyuan(xiaoxie) + biaolie.Bls.Fubianma(xiaoxie)
+}
+func (sjkmhsydata *Sjkmhsydata) DtziyuanLujing(xiaoxie bool) string {
+	return biaolie.Bls.Dtziyuan(xiaoxie) + biaolie.Bls.Lujing(xiaoxie)
+}
+func (sjkmhsydata *Sjkmhsydata) DtziyuanChuangjianriqi(xiaoxie bool) string {
+	return biaolie.Bls.Dtziyuan(xiaoxie) + biaolie.Bls.Xiugairiqi(xiaoxie)
+}
+func (sjkmhsydata *Sjkmhsydata) DtziyuanXiugairiqi(xiaoxie bool) string {
+	return biaolie.Bls.Dtziyuan(xiaoxie) + biaolie.Bls.Xiugairiqi(xiaoxie)
+}
+func (sjkmhsydata *Sjkmhsydata) DtziyuanBiaoji(xiaoxie bool) string {
+	return biaolie.Bls.Dtziyuan(xiaoxie) + biaolie.Bls.Biaoji(xiaoxie)
 }
