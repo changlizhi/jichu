af8cf57083628f124bd6c2b20a91bf73d3c74d68
changlz <changlizhi@aliyun.com>
2017-09-26 10:54:51 +0800

    组装bianma方法的生成

--- scfortests/Shengchengforteststest.go
+++ scfortests/Shengchengforteststest.go
@@ -4,18 +4,19 @@ import (
 	"bytes"
 	"changliang/zf"
 	"changliang/zfzhi"
-	"gongju"
-	"strings"
 	"changliang/zh"
-	"os"
+	"gongju"
 	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func importforteststest(mkv string, buffer *bytes.Buffer) {
 	buffer.WriteString(zf.Zfs.Import(true))
 	buffer.WriteString(zfzhi.Zhi.Xkhz() + zfzhi.Zhi.Hhf())
 	//"changliang/fanshe"
-	cfstr := zfzhi.Zhi.Syh() + zf.Zfs.Changliang(true) + zfzhi.Zhi.Xx() + zf.Zfs.Fanshe(true) + zfzhi.Zhi.Syh() + zfzhi.Zhi.Hhf()
+	cfstr := zfzhi.Zhi.Syh() + zf.Zfs.Changliang(true) + zfzhi.Zhi.Xx() +
+		zf.Zfs.Fanshe(true) + zfzhi.Zhi.Syh() + zfzhi.Zhi.Hhf()
 	buffer.WriteString(cfstr)
 	//"changliang/zf"
 	zstr := zfzhi.Zhi.Syh() + zf.Zfs.Changliang(true) + zfzhi.Zhi.Xx() +
@@ -58,7 +59,7 @@ func zzcs(houzhui string, bmx string, buffer *bytes.Buffer) {
 	buffer.WriteString(zfzhi.Zhi.Hhf() + zfzhi.Zhi.Xkhy() + zfzhi.Zhi.Hhf())
 
 	//log.Println(jiegouti)
-	lj := zh.Zhs.Logszh(zf.Zfs.Jiegouti(true) + houzhui) + zfzhi.Zhi.Hhf()
+	lj := zh.Zhs.Logszh(zf.Zfs.Jiegouti(true)+houzhui) + zfzhi.Zhi.Hhf()
 	buffer.WriteString(lj)
 }
 func testzuzhuangjiegou(bianma string, buffer *bytes.Buffer) {
@@ -72,11 +73,42 @@ func testzuzhuangjiegou(bianma string, buffer *bytes.Buffer) {
 	buffer.WriteString(csstr)
 	buffer.WriteString(zfzhi.Zhi.Dkhz() + zfzhi.Zhi.Hhf())
 	zzcs(zfzhi.Zhi.Kzf(), bmx, buffer)
-	zzcs(zf.Zfs.Yige(true) + zf.Zfs.String(true), bmx, buffer)
+	zzcs(zf.Zfs.Yige(true)+zf.Zfs.String(true), bmx, buffer)
 
 	buffer.WriteString(zfzhi.Zhi.Hhf() + zfzhi.Zhi.Dkhy() + zfzhi.Zhi.Hhf())
 
 }
+
+func testzuzhuangbianmacs(houzhui string, buffer *bytes.Buffer) {
+	//func TestZuzhuangbianma+houzhui
+	funstr := zf.Zfs.Func(true) + zfzhi.Zhi.Kgf() + zf.Zfs.Test(false) +
+		zf.Zfs.Zuzhuang(false) + zf.Zfs.Bianma(true) + houzhui
+	buffer.WriteString(funstr)
+	//(t *testing.T)
+	csstr := zfzhi.Zhi.Xkhz() + zf.Zfs.T(true) + zfzhi.Zhi.Kgf() +
+		zfzhi.Zhi.Xh() + zf.Zfs.Testing(true) + zfzhi.Zhi.Dh() + zf.Zfs.T(false) + zfzhi.Zhi.Xkhy()
+	buffer.WriteString(csstr)
+	buffer.WriteString(zfzhi.Zhi.Dkhz() + zfzhi.Zhi.Hhf())
+
+	//jiegouti := fortests.Zuzhuangbianma+houzhui
+	jgt := zf.Zfs.Jiegouti(true) + zfzhi.Zhi.Mh() + zfzhi.Zhi.Dyh() +
+		zf.Zfs.Fortests(true) + zfzhi.Zhi.Dh() + zf.Zfs.Zuzhuang(false) + zf.Zfs.Bianma(true) + houzhui
+	buffer.WriteString(jgt)
+	// (zf.Zfs.Kus(false) + zf.Zfs.Bianma(false) + fanshe.Fangfaming(false))
+	jgtcs := zfzhi.Zhi.Xkhz() + zh.Zhs.Zfszhfalse(zf.Zfs.Kus(false)) + zfzhi.Zhi.Jia() +
+		zh.Zhs.Zfszhfalse(zf.Zfs.Bianma(false)) + zfzhi.Zhi.Jia() +
+		zf.Zfs.Fanshe(true) + zfzhi.Zhi.Dh() + zf.Zfs.Fangfaming(false) +
+		zfzhi.Zhi.Xkhz() + zf.Zfs.False(true) + zfzhi.Zhi.Xkhy() +
+		zfzhi.Zhi.Xkhy() + zfzhi.Zhi.Hhf()
+	buffer.WriteString(jgtcs)
+	//log.Println(jiegouti)
+	lstr := zh.Zhs.Logszh(zf.Zfs.Jiegouti(true)) + zfzhi.Zhi.Hhf()
+	buffer.WriteString(lstr)
+
+	buffer.WriteString(zfzhi.Zhi.Hhf() + zfzhi.Zhi.Dkhy() + zfzhi.Zhi.Hhf())
+
+}
+
 func Shengchengforteststest() {
 	mkarr := gongju.Mokuaimingsarr
 	mks := gongju.Mokuaimings
@@ -89,9 +121,12 @@ func Shengchengforteststest() {
 			buffer.WriteString(pac)
 			importforteststest(mkv, buffer)
 			testzuzhuangjiegou(bk, buffer)
+			testzuzhuangbianmacs(zfzhi.Zhi.Kzf(), buffer)
+			testzuzhuangbianmacs(zf.Zfs.String(true), buffer)
 
 			dir := gongju.Getgopath() + zfzhi.Zhi.Xx() + mkv + zfzhi.Zhi.Xx() + zf.Zfs.Tests(true)
-			path := dir + zfzhi.Zhi.Xx() + bk + zf.Zfs.Fortests(true) + zfzhi.Zhi.Xhx() + zf.Zfs.Test(true) + zfzhi.Zhi.Dh() + zf.Zfs.Go(true)
+			path := dir + zfzhi.Zhi.Xx() + bk + zf.Zfs.Fortests(true) +
+				zfzhi.Zhi.Xhx() + zf.Zfs.Test(true) + zfzhi.Zhi.Dh() + zf.Zfs.Go(true)
 			os.MkdirAll(dir, os.ModePerm)
 			ioutil.WriteFile(path, buffer.Bytes(), os.ModePerm)
 		}
