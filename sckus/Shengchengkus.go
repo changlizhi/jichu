a2f6ec1a12f9f0153f09da828e4e5ab128dede7c
changlz <changlizhi@aliyun.com>
2017-10-20 13:57:59 +0800

    增加条件：youxiaoxing=1

--- sckus/Shengchengkus.go
+++ sckus/Shengchengkus.go
@@ -332,6 +332,13 @@ func chaxunquanbu(mokuai string, biao string, buffer *bytes.Buffer) {
 		zf.Zfs.QueryTable(false) + zfzhi.Zhi.Xkhz() + zh.Zhs.Zfszhtrue(biao) +
 		zfzhi.Zhi.Xkhy() + zfzhi.Zhi.Hhf()
 	buffer.WriteString(qsstr)
+	//queryseter = queryseter.Filter(zf.Zfs.Youxiaoxing(true), zfzhi.Zhi.Shuzi1w())
+	qqfy := zf.Zfs.QuerySeter(true) + zfzhi.Zhi.Dyh() + zf.Zfs.QuerySeter(true) +
+		zfzhi.Zhi.Dh() + zf.Zfs.Filter(false) + zfzhi.Zhi.Xkhz() +
+		zh.Zhs.Zfszhtrue(zf.Zfs.Youxiaoxing(false)) + zfzhi.Zhi.Dou() +
+		zh.Zhs.Zhiszh(zf.Zfs.Shuzi1w(false)) + zfzhi.Zhi.Xkhy() + zfzhi.Zhi.Hhf()
+	buffer.WriteString(qqfy)
+
 	// queryseter = queryseter.Limit(zfzhi.Zhi.Shuzifu1())
 	listr := zf.Zfs.QuerySeter(true) + zfzhi.Zhi.Dyh() + zf.Zfs.QuerySeter(true) +
 		zfzhi.Zhi.Dh() + zf.Zfs.Limit(false) + zfzhi.Zhi.Xkhz() +
