07b91cda9a12d24fdde49c2934363adeeed82c9e
changlz <changlizhi@aliyun.com>
2017-10-22 14:33:24 +0800

    增加表数据结构的sql生成

--- scsql/Shengchengsqlxin.go
+++ scsql/Shengchengsqlxin.go
@@ -33,7 +33,7 @@ func Shengchengchuangjian() {
 				zf.Zfs.Struct(true) + zfzhi.Zhi.Dkhz() + zfzhi.Zhi.Dkhy() + zfzhi.Zhi.Hhf()
 			bf.WriteString(tcbf)
 
-			//CREATE TABLE `mkv`.`b` (
+			//CREATE TABLE `mkv`.`bshuju` (
 			crestr := zf.Zfs.Create(true) + zfzhi.Zhi.Kgf() + zf.Zfs.Table(true) +
 				zfzhi.Zhi.Kgf() + zfzhi.Zhi.Yzb() + mkv + zfzhi.Zhi.Yzb() +
 				zfzhi.Zhi.Dh() + zfzhi.Zhi.Yzb() + b + zf.Zfs.Shuju(true) + zfzhi.Zhi.Yzb()
@@ -85,8 +85,56 @@ func Shengchengchuangjian() {
 				zfzhi.Zhi.Xhx() + zf.Zfs.Ci(true) + zfzhi.Zhi.Dyhe() + zfzhi.Zhi.Hhf()
 			buffer.WriteString(cug)
 			//ENGINE=InnoDB;
-			ei := zf.Zfs.Engine(true) + zfzhi.Zhi.Dyh() + zf.Zfs.InnoDB(true) + zfzhi.Zhi.Fh() + zfzhi.Zhi.Hhf()
+			ei := zf.Zfs.Engine(true) + zfzhi.Zhi.Dyh() + zf.Zfs.InnoDB(true) +
+				zfzhi.Zhi.Fh() + zfzhi.Zhi.Hhf() + zfzhi.Zhi.Hhf()
 			buffer.WriteString(ei)
+
+
+			//create table `mkv`.`bbiao` (
+			crestrbiao := zf.Zfs.Create(true) + zfzhi.Zhi.Kgf() + zf.Zfs.Table(true) +
+				zfzhi.Zhi.Kgf() + zfzhi.Zhi.Yzb() + mkv + zfzhi.Zhi.Yzb() +
+				zfzhi.Zhi.Dh() + zfzhi.Zhi.Yzb() + b + zf.Zfs.Biao(true) + zfzhi.Zhi.Yzb()
+			buffer.WriteString(crestrbiao)
+			buffer.WriteString(zfzhi.Zhi.Xkhz() + zfzhi.Zhi.Hhf())
+
+			for _, sjzd := range gongju.Fanshejichubiao() {
+				cdbiao := gongju.Liechangdu(sjzd)
+				cdzwbiao := strconv.Itoa(cdbiao)
+				if sjzd == zf.Zfs.Id(false) {
+					//Id int(10) auto_increment comment '主键',
+					vcbiao := zfzhi.Zhi.Yzb() + sjzd + zfzhi.Zhi.Yzb() + zfzhi.Zhi.Kgf() + zf.Zfs.Int(true) +
+						zfzhi.Zhi.Xkhz() + cdzwbiao + zfzhi.Zhi.Xkhy() + zfzhi.Zhi.Kgf() +
+						zf.Zfs.Auto(true) + zfzhi.Zhi.Xhx() + zf.Zfs.Increment(true) +
+						zfzhi.Zhi.Kgf() + zf.Zfs.Comment(true) + zfzhi.Zhi.Kgf() +
+						zfzhi.Zhi.Dyhe() + gongju.Zhongwen(sjzd) + zfzhi.Zhi.Dyhe() + zfzhi.Zhi.Dou() + zfzhi.Zhi.Hhf()
+					buffer.WriteString(vcbiao)
+				} else {
+					vcbiao := zfzhi.Zhi.Yzb() + sjzd + zfzhi.Zhi.Yzb() + zfzhi.Zhi.Kgf() + zf.Zfs.Varchar(true) +
+						zfzhi.Zhi.Xkhz() + cdzwbiao + zfzhi.Zhi.Xkhy() +
+						zfzhi.Zhi.Kgf() + zf.Zfs.Comment(true) + zfzhi.Zhi.Kgf() +
+						zfzhi.Zhi.Dyhe() + gongju.Zhongwen(sjzd) + zfzhi.Zhi.Dyhe() + zfzhi.Zhi.Dou() + zfzhi.Zhi.Hhf()
+					buffer.WriteString(vcbiao)
+				}
+			}
+			//primary key (`id`)
+			pkstrbiao := zf.Zfs.Primary(true) + zfzhi.Zhi.Kgf() + zf.Zfs.Key(true) +
+				zfzhi.Zhi.Xkhz() + zfzhi.Zhi.Yzb() + zf.Zfs.Id(false) + zfzhi.Zhi.Yzb() +
+				zfzhi.Zhi.Xkhy() + zfzhi.Zhi.Hhf()
+			buffer.WriteString(pkstrbiao)
+			buffer.WriteString(zfzhi.Zhi.Hhf() + zfzhi.Zhi.Xkhy() + zfzhi.Zhi.Hhf())
+			//COMMENT='时间'
+			ctbiao := zf.Zfs.Comment(true) + zfzhi.Zhi.Dyh() + zfzhi.Zhi.Dyhe() +
+				gongju.Zhongwen(b) + zfzhi.Zhi.Dyhe() + zfzhi.Zhi.Hhf()
+			buffer.WriteString(ctbiao)
+			//COLLATE='utf8_general_ci'
+			cugbiao := zf.Zfs.Collate(true) + zfzhi.Zhi.Dyh() + zfzhi.Zhi.Dyhe() +
+				zf.Zfs.Utf8(true) + zfzhi.Zhi.Xhx() + zf.Zfs.General(true) +
+				zfzhi.Zhi.Xhx() + zf.Zfs.Ci(true) + zfzhi.Zhi.Dyhe() + zfzhi.Zhi.Hhf()
+			buffer.WriteString(cugbiao)
+			//ENGINE=InnoDB;
+			eibiao := zf.Zfs.Engine(true) + zfzhi.Zhi.Dyh() + zf.Zfs.InnoDB(true) +
+				zfzhi.Zhi.Fh() + zfzhi.Zhi.Hhf() + zfzhi.Zhi.Hhf()
+			buffer.WriteString(eibiao)
 		}
 		//log.Println(bf.String())
 		dir := gongju.Getgopath() + zfzhi.Zhi.Xx() + mkv +
